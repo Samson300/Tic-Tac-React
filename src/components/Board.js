04f37e44f3501887f6965e0dddc2ac1154b332a7
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-09-16 16:14:36 -0400

    changes Box component to Board

---
+++ src/components/Board.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Board extends Component{
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
